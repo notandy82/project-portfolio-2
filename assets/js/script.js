f89ae5c5ea1a268d5a08e572b429d980efb09b03
notandy82 <notandystanek@gmail.com>
2022-02-01 15:49:45 +0000

    Add onclick function to buttons to remove introduction elements

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+function onePlayer() {
+    var game1 = document.getElementById("splash-page");
+    game1.remove();
+    console.log("launch 1 player game");
+}
+
+function twoPlayer() {
+    var game2 = document.getElementById("splash-page");
+    game2.remove();
+    console.log("launch 2 player game")
+}
