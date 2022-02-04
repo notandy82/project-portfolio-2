ac1b11cc25483b83f9cc1589e434da44d17beb2a
notandy82 <notandystanek@gmail.com>
2022-02-04 16:38:45 +0000

    Add functions for computer choice and single player choice

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// Functions to select game type
 function onePlayer() {
     document.getElementById("splash").style.display="none";
     document.getElementById("oneplayer").style.display="block";
@@ -8,4 +9,42 @@ function twoPlayer() {
     document.getElementById("splash").style.display="none";
     document.getElementById("twoplayer").style.display="block";
     console.log("launch 2 player game")
+}
+
+// One player game functions
+var playerChoice;
+var compChoice;
+
+// Function for computer choice
+function compThrow() {
+        let compRandom = Math.floor(Math.random() * 3) + 1;
+        if (compRandom == 1) {
+        compThrow = "Rock";
+    } else if (compRandom == 2) {
+        compChoice = "Scissors";
+    } else {
+        compChoice = "Dynamite";
+    }
+    console.log(compChoice);
+}
+
+function playerRockThrow() {
+    playerChoice = "Rock";
+    console.log(playerChoice);
+    compThrow();
+    
+}
+
+function playerScissorsThrow() {
+    playerChoice = "Scissors";
+    console.log(playerChoice);
+    compThrow();
+    
+}
+
+function playerDynamiteThrow() {
+    playerChoice = "Dynamite";
+    console.log(playerChoice);
+    compThrow();
+    
 }
